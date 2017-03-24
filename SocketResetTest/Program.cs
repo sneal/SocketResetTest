5cf49513cbf64b8f2e6a3d3f2f3a9ee7cf08d6fd
Shawn Neal <sneal@sneal.net>
2017-03-24 13:19:43 -0700

    Add initial app that can GET google.com

---
+++ SocketResetTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+
+namespace SocketResetTest
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting SocketReset Test");
+            var httpStatus = SubmitRequest("http://www.google.com");
+            Console.WriteLine($"Received HTTP status: {httpStatus}");
+
+            // just wait forever
+            Console.ReadLine();
+        }
+		
+		private static HttpStatusCode SubmitRequest(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+            request.Method = "GET";
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
+                {
+                    return response.StatusCode;
+                }
+            }
+            catch (WebException ex)
+            {
+                using (HttpWebResponse response = (HttpWebResponse)ex.Response)
+                {
+                    return response.StatusCode;
+                }
+            }
+        }
+    }
+}
